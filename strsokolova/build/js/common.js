94a2d453e60d1d87d77736b8eca102f56123eaa1
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2020-07-10 18:10:46 +0300

    eddited

--- strsokolova/build/js/common.js
+++ strsokolova/build/js/common.js
@@ -2,16 +2,23 @@
 
 $(document).ready(function () {
   hoverTouchEvents();
-}); //hover/touch custom events
+  navigation();
+});
+var brakepoints = {
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1280
+}; //hover/touch custom events
 
 function hoverTouchEvents() {
-  $(document).on('mouseenter mouseleave touchstart touchend mousedown mouseup', 'a,button,label,input,textarea,.js-touch-hover', function (event) {
+  $(document).on('mouseenter mouseleave touchstart touchend mousedown mouseup contextmenu', 'a,button,label,input,textarea,.js-touch-hover', function (event) {
     var $target = $(this); //mobile events
 
     if (!device.desktop()) {
       if (event.type == 'touchstart') {
         $target.addClass('touch');
-      } else if (event.type == 'touchend') {
+      } else if (event.type == 'touchend' || event.type == 'contextmenu') {
         $target.removeClass('touch');
       }
     } //desktop events
@@ -27,5 +34,30 @@ function hoverTouchEvents() {
           $target.removeClass('mousedown');
         }
       }
+  });
+}
+
+function navigation() {
+  var flag;
+
+  function checkPosition() {
+    var $buttons = $('.header__buttons-group'),
+        $mobileContainer = $('.header__top .container'),
+        $desktopContainer = $('.header__bottom .container');
+
+    if ($(window).width() < brakepoints.md && !flag) {
+      $buttons.appendTo($mobileContainer);
+      flag = true;
+      console.log('1');
+    } else if ($(window).width() >= brakepoints.md && flag) {
+      $buttons.appendTo($desktopContainer);
+      flag = false;
+      console.log('2');
+    }
+  }
+
+  checkPosition();
+  $(window).on('resize', function () {
+    checkPosition();
   });
 }
--- strsokolova/src/js/common.js
+++ strsokolova/src/js/common.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
   hoverTouchEvents();
+  navigation();
 })
+
+const brakepoints = {
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1280
+}
 
 
 //hover/touch custom events
 function hoverTouchEvents() {
-  $(document).on('mouseenter mouseleave touchstart touchend mousedown mouseup', 'a,button,label,input,textarea,.js-touch-hover', function(event) {
+  $(document).on('mouseenter mouseleave touchstart touchend mousedown mouseup contextmenu', 'a,button,label,input,textarea,.js-touch-hover', function(event) {
     let $target = $(this);
     //mobile events
     if(!device.desktop()) {
       if(event.type=='touchstart') {
         $target.addClass('touch');
       } 
-      else if(event.type=='touchend') {
+      else if(event.type=='touchend' || event.type=='contextmenu') {
         $target.removeClass('touch');
       }
     } 
@@ -35,3 +43,29 @@ function hoverTouchEvents() {
   })
 }
 
+
+function navigation() {
+  let flag;
+  
+  function checkPosition() {
+    let $buttons = $('.header__buttons-group'),
+        $mobileContainer = $('.header__top .container'),
+        $desktopContainer = $('.header__bottom .container');
+
+    if($(window).width()<brakepoints.md && !flag) {
+      $buttons.appendTo($mobileContainer);
+      flag = true;
+      console.log('1')
+    } 
+    else if($(window).width()>=brakepoints.md && flag) {
+      $buttons.appendTo($desktopContainer);
+      flag = false;
+      console.log('2')
+    }
+  }
+
+  checkPosition();
+  $(window).on('resize', function() {
+    checkPosition();
+  })
+}
