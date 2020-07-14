84c9bf748040568f690bbbd8a50e8440afee69ba
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2020-07-14 17:11:07 +0300

    eddited

--- yes-95/src/js/common.js
+++ yes-95/src/js/common.js
@@ -25,5 +25,68 @@ $wrapper.style.minHeight = `${document.documentElement.clientHeight}px`;
 
 /*==== Start =====*/
 document.addEventListener("DOMContentLoaded", ()=>{
-
+  touchHoverEvents();
 });
+
+function touchHoverEvents() {
+  document.addEventListener('touchstart', event);
+  document.addEventListener('touchend', event);
+
+  document.addEventListener('mouseover', event);
+  document.addEventListener('mouseout', event);
+
+  document.addEventListener('mousedown', event);
+  document.addEventListener('mouseup', event);
+  
+  document.addEventListener('contextmenu', event)
+
+  let $elements = 'a, button, label, input, textarea',
+      touch,
+      timeout;
+
+  function event(event) {
+    if(event.target!==document) {
+      let $target = event.target.closest($elements);
+      if($target!==null) {
+
+        if(event.type=='touchstart') {
+          for(let $this of document.querySelectorAll($elements)) {
+            $this.classList.remove('touch');
+          }
+          touch = true;
+          clearTimeout(timeout)
+          $target.classList.add('touch');
+        } else if(event.type=='touchend') {
+          $target.classList.remove('touch');
+          timeout = setTimeout(()=>{
+            touch = false;
+          }, 1000)
+        } else if(event.type=='contextmenu') {
+          $target.classList.remove('touch');
+          timeout = setTimeout(()=>{
+            touch = false;
+          }, 1000)
+        }
+
+        if(event.type=='mouseover' && !touch) {
+          $target.classList.add('hover');
+        } else if(event.type=='mouseout' && !touch) {
+          $target.classList.remove('hover');
+          $target.classList.remove('focus');
+        }
+        
+        if(event.type=='mousedown') {
+          $target.classList.add('focus');
+        } else if(event.type=='mouseup') {
+          $target.classList.remove('focus');
+        }
+
+      } else {
+        for(let $this of document.querySelectorAll($elements)) {
+          $this.classList.remove('touch');
+        }
+      }
+    }
+  }
+
+}
