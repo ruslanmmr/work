399d2a80bea06d1297cdac0cb9f8141a9de68e43
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2020-07-15 14:32:50 +0300

    eddited

--- yes-95/src/js/common.js
+++ yes-95/src/js/common.js
@@ -1,23 +1,23 @@
 import device from "current-device";
 import { disablePageScroll, enablePageScroll } from 'scroll-lock';
-
-//lazylaod
-import 'lazysizes';
-lazySizes.cfg.lazyClass = 'lazy';
-lazySizes.cfg.loadedClass = 'loaded';
-lazySizes.cfg.preloadAfterLoad = true;
-document.addEventListener('lazybeforeunveil', function(e){
-  let el = e.target.tagName,
-      bg = e.target.getAttribute('data-src');
-  if(el!=='IMG') {
-    let bg = e.target.getAttribute('data-src');
-    e.target.style.backgroundImage = 'url(' + bg + ')';
-  }
-});
+import Inputmask from "inputmask";
 
 const $wrapper = document.querySelector('.wrapper');
 //set min-height
 $wrapper.style.minHeight = `${document.documentElement.clientHeight}px`;
+window.addEventListener('resize', ()=>{
+  $wrapper.style.minHeight = `${document.documentElement.clientHeight}px`;
+})
+//inputmask
+window.mask = Inputmask({
+  mask: "+7 999 999-9999",
+  showMaskOnHover: false,
+  clearIncomplete: false
+}).mask('[name="phone"]');
+
+
+
+
 
 
 /*==== Start =====*/
@@ -26,6 +26,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
   popups();
 });
 
+
+//custom touch/hover events
 function touchHoverEvents() {
   document.addEventListener('touchstart', event);
   document.addEventListener('touchend', event);
