7e63b071bca01ad07ca30570aa9e0b6666fcbf86
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2020-07-16 16:31:21 +0300

    eddited

--- jdmcars/src/js/scripts.js
+++ jdmcars/src/js/scripts.js
@@ -1,41 +1,46 @@
+//add lazy backgrounds
+document.addEventListener('lazybeforeunveil', function(e){
+  let el = e.target.tagName,
+      bg = e.target.getAttribute('data-src');
+  if(el!=='IMG') {
+    let bg = e.target.getAttribute('data-src');
+    e.target.style.backgroundImage = 'url(' + bg + ')';
+  }
+});
+
 
 $(document).ready(function() {
-  lazyBackgrounds();
   mainSlider();
   infoTabs();
 })
 
-//lazy backgrounds
-function lazyBackgrounds() {
-  //add lazy backgrounds
-  document.addEventListener('lazybeforeunveil', function(e){
-    let el = e.target.tagName,
-        bg = e.target.getAttribute('data-src');
-    if(el!=='IMG') {
-      let bg = e.target.getAttribute('data-src');
-      e.target.style.backgroundImage = 'url(' + bg + ')';
-    }
-  });
-}
 //slider
 function mainSlider() {
-  let $slider = $('.home-slider-main'),
-      $navslider = $('.home-slider-nav');
+  let $slider = $('.home-slider__container');
   if($slider.length) {
     $slider.slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      asNavFor: $navslider
-    });
-    $navslider.slick({
       slidesToShow: 3,
-      slidesToScroll: 1,
-      asNavFor: $slider,
+      slidesToScroll: 3,
       dots: true,
-      arrows: false,
-      centerMode: true,
-      focusOnSelect: true
+      arrows: true,
+      prevArrow: '<div class="home-slider__arrow home-slider__arrow-prev"><svg viewBox="0 0 27 47" fill="#fff"><path d="M26.8996 6.75L10.1496 23.5L26.8996 40.25L23.5496 46.95L0.0996094 23.5L23.5496 0.0500031L26.8996 6.75Z"/></svg></div>',
+      nextArrow: '<div class="home-slider__arrow home-slider__arrow-next"><svg viewBox="0 0 27 47" fill="#fff"><path d="M0.1,40.2l16.7-16.8L0.1,6.8l3.4-6.7l23.4,23.4L3.4,47L0.1,40.2z"/></svg></div>',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     });
   }
 }
