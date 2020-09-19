d47ac128ca74fd3497c22efa5b320def81802444
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2020-09-19 22:42:52 +0300

    eddited

--- bunnylove/src/js/new.js
+++ bunnylove/src/js/new.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
   mobileSearch();
   header();
+  slider.init();
 })
+
+const brakepoints = {
+  xs: 576,
+  sm: 768,
+  md: 1024,
+  lg: 1200
+}
 
 function mobileSearch() {
   let $open = $('.mobile-search-open'),
@@ -32,5 +40,80 @@ function header() {
     } else {
       $header.removeClass('active');
     }
+  }
+}
+
+let slider = {
+  el: $('.slider'),
+  init: function() {
+    slider.el.each(function () {
+      let slideCount = 1,
+          slideCountLg = 1,
+          slideCountMd = 1,
+          slideCountSm = 1,
+          slideCountXs = 1,
+          arrows = false,
+          dots = false,
+          centerMode = false,
+          autoplay = false;
+      
+      if($(this).is('.slider_dots')) {
+        dots=true;
+      } 
+      if($(this).is('.blog-slider')) {
+        slideCount = 3;
+        slideCountLg = 3;
+        slideCountMd = 2;
+        slideCountSm = 2;
+        slideCountXs = 1;
+        initSlider($(this));
+      } 
+
+      function initSlider($target) {
+        $target.slick({
+          rows: 0,
+          infinite: true,
+          dots: dots,
+          arrows: arrows,
+          speed: 500,
+          centerMode: centerMode,
+          slidesToShow: slideCount,
+          slidesToScroll: slideCount,
+          autoplay: autoplay,
+          autoplaySpeed: 5000,
+          responsive: [{
+              breakpoint: brakepoints.lg,
+              settings: {
+                slidesToShow: slideCountLg,
+                slidesToScroll: slideCountLg
+              }
+            },
+            {
+              breakpoint: brakepoints.md,
+              settings: {
+                slidesToShow: slideCountMd,
+                slidesToScroll: slideCountMd
+              }
+            },
+            {
+              breakpoint: brakepoints.sm,
+              settings: {
+                slidesToShow: slideCountSm,
+                slidesToScroll: slideCountSm
+              }
+            },
+            {
+              breakpoint: brakepoints.xs,
+              settings: {
+                slidesToShow: slideCountXs,
+                slidesToScroll: slideCountXs
+              }
+            }
+          ]
+        });
+      }
+    
+    });
+
   }
 }
