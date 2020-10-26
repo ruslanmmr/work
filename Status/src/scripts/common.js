c19e971003bb322690260df549e07f1b1a8ec683
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2020-10-26 18:28:47 +0200

    eddited

---
+++ Status/src/scripts/common.js
@@ -0,0 +1,393 @@
+lazySizes.cfg.init = false;
+const brakepoints = {
+  xs: 576,
+  sm: 768,
+  xl: 1024,
+  lg: 1280
+}
+
+$(document).ready(function(){
+  TouchHoverEvents.init();
+  select.init();
+  slider.init();
+  nav.init();
+  lazy();
+  toggle();
+  up();
+  tabs();
+  window.mask = Inputmask({
+    mask: "+7 999 999-9999",
+    showMaskOnHover: false,
+    clearIncomplete: false
+  }).mask('.input__phone');
+  fancy();
+})
+
+//hover/touch custom events
+const TouchHoverEvents = {
+  targets: 'a[class], button, label, tr, .jsTouchHover, .selectric-items li',
+  touched: false,
+  touchEndDelay: 100, //ms
+  init: function() {
+    document.addEventListener('touchstart',  (event)=>{this.events(event)});
+    document.addEventListener('touchend',    (event)=>{this.events(event)});
+    document.addEventListener('mouseenter',  (event)=>{this.events(event)},true);
+    document.addEventListener('mouseleave',  (event)=>{this.events(event)},true);
+    document.addEventListener('mousedown',   (event)=>{this.events(event)});
+    document.addEventListener('mouseup',     (event)=>{this.events(event)});
+    document.addEventListener('contextmenu', (event)=>{this.events(event)});
+  },
+  events: function(event) {
+    let $targets = [];
+    $targets[0] = event.target!==document?event.target.closest(this.targets):null;
+    let $element = $targets[0], i = 0;
+
+    while($targets[0]) {
+      $element = $element.parentNode;
+      if($element!==document) {
+        if($element.matches(this.targets)) {
+          i++;
+          $targets[i] = $element;
+        }
+      } 
+      else {
+        break;
+      }
+    }
+
+    //touchstart
+    if(event.type=='touchstart') {
+      this.touched = true;
+      if(this.timeout) clearTimeout(this.timeout);
+      if($targets[0]) {
+        for(let $target of document.querySelectorAll(this.targets)) $target.classList.remove('touch');
+        for(let $target of $targets) $target.classList.add('touch');
+      }
+    } 
+    //touchend
+    else if(event.type=='touchend' || (event.type=='contextmenu' && this.touched)) {
+      this.timeout = setTimeout(() => {this.touched = false}, 500);
+      if($targets[0]) {
+        setTimeout(()=>{
+          for(let $target of $targets) {
+            $target.dispatchEvent(new CustomEvent("customTouchend"));
+            $target.classList.remove('touch');
+          }
+        }, this.touchEndDelay)
+      }
+    } 
+    
+    //mouseenter
+    if(event.type=='mouseenter' && !this.touched && $targets[0] && $targets[0]==event.target) {
+      $targets[0].classList.add('hover');
+    }
+    //mouseleave
+    else if(event.type=='mouseleave' && !this.touched && $targets[0] && $targets[0]==event.target) {
+      $targets[0].classList.remove('hover', 'focus');
+    }
+    //mousedown
+    if(event.type=='mousedown' && !this.touched && $targets[0]) {
+      $targets[0].classList.add('focus');
+    } 
+    //mouseup
+    else if(event.type=='mouseup' && !this.touched  && $targets[0]) {
+      $targets[0].classList.remove('focus');
+    }
+  }
+}
+
+//lazyloading
+function lazy() {
+  //add backgrounds
+  document.addEventListener('lazybeforeunveil', function(e){
+    let el = e.target.tagName,
+        bg = e.target.getAttribute('data-src');
+    if(el!=='IMG') {
+      let bg = e.target.getAttribute('data-src');
+      e.target.style.backgroundImage = `url('${bg}')`;
+    }
+  });
+  lazySizes.init();
+}
+
+//select
+let select = {
+  init: function() {
+    this.items = $('select');
+    if(this.items.length) {
+      this.items.selectric({
+        disableOnMobile: false,
+        nativeOnMobile: false,
+        arrowButtonMarkup: '<svg class="icon" width="10" height="5" viewBox="0 0 10 5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 5L0 7.57103e-07L10 0L5 5Z" fill="#222222"/></svg>'});
+    }
+  }
+}
+
+const slider = {
+  arrow: '<svg class="icon" width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L14 14L1 27" stroke="currentColor" stroke-width="2"/></svg>',
+  init: function() {
+    this.el = $('.slider');
+    slider.el.each(function () {
+      let slideCount = 1,
+          slideCountLg = 1,
+          slideCountMd = 1,
+          slideCountSm = 1,
+          slideCountXs = 1,
+          arrows = false,
+          dots = false,
+          centerMode = false,
+          autoplay = false,
+          nextArrow = `<button type="button" class="slider__next">${slider.arrow}</button>`,
+          prevArrow = `<button type="button" class="slider__prev">${slider.arrow}</button>`;
+      
+      
+      if($(this).is('.slider_3n')) {
+        slideCount = 3;
+        slideCountLg = 2;
+        slideCountMd = 2;
+        slideCountSm = 2;
+        slideCountXs = 1;
+        arrows = true;
+        dots = true;
+        initSlider($(this));
+      } 
+
+      function initSlider($target) {
+        $target.slick({
+          rows: 0,
+          infinite: true,
+          dots: dots,
+          arrows: arrows,
+          nextArrow: nextArrow,
+          prevArrow: prevArrow,
+          speed: 500,
+          centerMode: centerMode,
+          slidesToShow: slideCount,
+          slidesToScroll: slideCount,
+          autoplay: autoplay,
+          autoplaySpeed: 5000,
+          responsive: [{
+              breakpoint: brakepoints.lg,
+              settings: {
+                slidesToShow: slideCountLg,
+                slidesToScroll: slideCountLg
+              }
+            },
+            {
+              breakpoint: brakepoints.md,
+              settings: {
+                slidesToShow: slideCountMd,
+                slidesToScroll: slideCountMd
+              }
+            },
+            {
+              breakpoint: brakepoints.sm,
+              settings: {
+                slidesToShow: slideCountSm,
+                slidesToScroll: slideCountSm
+              }
+            },
+            {
+              breakpoint: brakepoints.xs,
+              settings: {
+                slidesToShow: slideCountXs,
+                slidesToScroll: slideCountXs
+              }
+            }
+          ]
+        });
+      }
+    
+    });
+
+  }
+}
+
+function toggle() {
+  let $section = $('.toggle-section'),
+      speed = 250;
+
+  $section.each(function() {
+    let $this = $(this),
+        $toggle = $this.children('.toggle-section__trigger'),
+        $content = $this.children('.toggle-section__content'),
+        state = $this.hasClass('active') ? true : false,
+        initialized;
+
+    $toggle.on('click', function() {
+      state = !state ? true : false;
+      check();
+    })
+    
+    if($this.is('[data-hover]')) {
+      let timeout;
+      
+      $toggle.add($content).on('mouseenter', function(event){
+        if(!TouchHoverEvents.touched) {
+          if(timeout) clearTimeout(timeout);
+          state=true;
+          check();
+        }
+      })
+
+      $toggle.add($content).on('mouseleave', function(event){
+        if(!TouchHoverEvents.touched) {
+          let delay;
+          if($(this).is($toggle)) {
+            delay=500;
+          } else {
+            delay=100;
+          }
+          timeout = setTimeout(()=>{
+            state=false;
+            check();
+          }, delay)
+        }
+      })
+
+    }
+
+    if($this.is('[data-out-hide]') || $this.is('[data-hover]')) {
+      $(document).on('click touchstart', function(event) {
+        let $target = $(event.target);
+        if(!$target.closest($content).length && !$target.closest($toggle).length && state) {
+          state=false;
+          check();
+        }
+      })
+    } 
+
+    function check() {
+      if(state) {
+        $this.add($content).add($toggle).addClass('active');
+        if($this.is('[data-slide]')) {
+          $content.slideDown(speed);
+        }
+      } 
+      else {
+        $this.add($toggle).add($content).removeClass('active');
+        if($this.is('[data-slide]')) {
+          if(initialized) {
+            $content.stop().slideUp(speed);
+          } else {
+            $content.hide(0);
+          }
+        }
+      }
+    }
+
+    check();
+
+    initialized=true;
+  })
+}
+
+function up() {
+  let $btn = $('.js-up');
+
+  $(window).on('scroll', function() {
+    let scroll = $(window).scrollTop();
+    if(scroll>$('.header').height()) {
+      $btn.addClass('active');
+    } else {
+      $btn.removeClass('active');
+    }
+  })
+
+  $btn.on('click', function(event) {
+    event.preventDefault();
+    $("html, body").animate({scrollTop:0}, 500);
+  })
+}
+
+function fancy() {
+  $.fancybox.defaults.i18n.ru = {
+    CLOSE       : 'Закрыть',
+    NEXT        : 'Следующий слайд',
+    PREV        : 'Предидущий слайд',
+    ERROR       : 'Ошибка загрузки, попробуйте позже.',
+    PLAY_START  : 'Запустить слайд-шоу',
+    PLAY_STOP   : 'Остановить слайд-шоу',
+    FULL_SCREEN : 'Полноэкранный режим',
+    THUMBS      : 'Миниатюры',
+    DOWNLOAD    : 'Загрузить',
+    SHARE       : 'Поделиться',
+    ZOOM        : 'Увеличить'
+  };
+  $.fancybox.defaults.lang = 'ru';
+  $.fancybox.defaults.loop = true;
+  $.fancybox.defaults.autoFocus = false;
+  $.fancybox.defaults.animationEffect = 'fade'
+  $.fancybox.defaults.backFocus = 'false'
+
+  $("[data-modal]").fancybox({
+    autoFocus: false,
+    smallBtn: true,
+    touch: false
+  });
+}
+
+function tabs() {
+  let $tablink = $('.services__link'),
+      $tabblock = $('.services-block'),
+      current = 0,
+      old;
+
+  let check = ()=> {
+    if(old!==undefined) {
+      $tabblock.eq(old).hide();
+      $tablink.eq(old).removeClass('active');
+    }
+    $tabblock.eq(current).fadeIn(500);
+    $tablink.eq(current).addClass('active');
+    old=current;
+  }
+
+  check();
+  $tablink.on('click', function(event) {
+    current = $tablink.index($(this));
+    check();
+  })
+}
+
+const nav = {
+  init: function() {
+    this.$nav = $('.mobile-nav');
+    this.$toggle = $('.nav-toggle');
+
+    this.$toggle.on('click', (event)=>{
+      event.preventDefault();
+      if(this.flag) {
+        this.close();
+      } else {
+        this.open();
+      }
+    })
+    
+    $(document).on('click touchstart', (event)=>{
+      if( $(event.target).closest('.mobile-nav__container').length==0 
+          && $(event.target).closest('.header').length==0
+          && $(event.target).closest(this.$toggle).length==0
+          && this.flag==true) {
+            this.close(); 
+      }
+    })
+
+  }, 
+
+  open: function() {
+    this.flag = true;
+    scrollLock.disablePageScroll();
+    $('header').addClass('header_nav-active')
+    this.$nav.addClass('active');
+    this.$toggle.addClass('active');
+  },
+
+  close: function() {
+    this.flag = false;
+    scrollLock.enablePageScroll();
+    $('header').removeClass('header_nav-active')
+    this.$nav.removeClass('active');
+    this.$toggle.removeClass('active');
+  }
+}
