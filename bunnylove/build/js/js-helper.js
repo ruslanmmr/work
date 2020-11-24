42689e37b6311866930c307c30671ab847b5cd0a
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2020-11-24 02:02:24 +0200

    eddited

--- bunnylove/build/js/js-helper.js
+++ bunnylove/build/js/js-helper.js
@@ -1,9 +1,43 @@
 "use strict";
 
-$(document).ready(function () {
-  var $toggle = $('.js-helper-module__trigger '),
-      $block = $('.js-helper-module');
-  $toggle.on('click', function () {
-    $block.toggleClass('active');
-  });
+window.addEventListener('load', function () {
+  Helper.init();
 });
+var Helper = {
+  init: function init() {
+    var _this = this;
+
+    this.$block = document.querySelector('.helper');
+    this.$trigger = this.$block.querySelector('.helper__trigger');
+    this.set_active_page();
+    this.$trigger.addEventListener('click', function () {
+      if (!_this.state) {
+        _this.open();
+      } else {
+        _this.close();
+      }
+    });
+  },
+  set_active_page: function set_active_page() {
+    var values = location.href.split('/'),
+        last_value = values[values.length - 1],
+        page = last_value == '' ? 'index.html' : last_value;
+    var $links = this.$block.querySelectorAll('a');
+    $links.forEach(function ($this) {
+      var href_values = $this.getAttribute('href').split('/'),
+          href_page = href_values[href_values.length - 1];
+
+      if (page == href_page) {
+        $this.classList.add('active');
+      }
+    });
+  },
+  open: function open() {
+    this.state = true;
+    this.$block.classList.add('active');
+  },
+  close: function close() {
+    this.state = false;
+    this.$block.classList.remove('active');
+  }
+};
