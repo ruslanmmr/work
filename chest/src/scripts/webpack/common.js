0b48982e9037c48693f4b3d8ece327c651a366a4
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2021-04-07 14:29:06 +0300

    eddited

---
+++ chest/src/scripts/webpack/common.js
@@ -0,0 +1,98 @@
+import {gsap} from "gsap";
+
+const $body = document.body;
+const $wrapper = document.querySelector('.wrapper');
+
+document.addEventListener('DOMContentLoaded', function(){
+  TouchHoverEvents.init();
+  windowSize.init();
+})
+
+window.onload = function() {
+  $body.classList.add('loaded');
+}
+
+const windowSize = {
+  init: function() {
+    let $el = document.createElement('div');
+    $el.style.cssText = 'position:fixed;height:100%;';
+    $body.insertAdjacentElement('beforeend', $el);
+    let h = $el.getBoundingClientRect().height;
+    
+    document.querySelectorAll('.screen').forEach($this => {
+      $this.style.height = `${h}px`;
+    })
+  }
+}
+
+const TouchHoverEvents = {
+  targets: 'a, button, label, tr, [data-touch-hover]',
+  touched: false,
+  touchEndDelay: 100, //ms
+  init: function() {
+    document.addEventListener('touchstart',  (event)=>{this.events(event)});
+    document.addEventListener('touchend',    (event)=>{this.events(event)});
+    document.addEventListener('mouseenter',  (event)=>{this.events(event)},true);
+    document.addEventListener('mouseleave',  (event)=>{this.events(event)},true);
+    document.addEventListener('mousedown',   (event)=>{this.events(event)});
+    document.addEventListener('mouseup',     (event)=>{this.events(event)});
+    document.addEventListener('contextmenu', (event)=>{this.events(event)});
+  },
+  events: function(event) {
+    let $targets = [];
+    $targets[0] = event.target!==document?event.target.closest(this.targets):null;
+    let $element = $targets[0], i = 0;
+
+    while($targets[0]) {
+      $element = $element.parentNode;
+      if($element!==document) {
+        if($element.matches(this.targets)) {
+          i++;
+          $targets[i] = $element;
+        }
+      } 
+      else {
+        break;
+      }
+    }
+
+    //touchstart
+    if(event.type=='touchstart') {
+      this.touched = true;
+      if(this.timeout) clearTimeout(this.timeout);
+      if($targets[0]) {
+        for(let $target of $targets) $target.setAttribute('data-touch', '');
+      }
+    } 
+    //touchend
+    else if(event.type=='touchend' || (event.type=='contextmenu' && this.touched)) {
+      this.timeout = setTimeout(() => {this.touched = false}, 500);
+      if($targets[0]) {
+        setTimeout(()=>{
+          for(let $target of $targets) {
+            $target.dispatchEvent(new CustomEvent("customTouchend"));
+            $target.removeAttribute('data-touch');
+          }
+        }, this.touchEndDelay)
+      }
+    } 
+    
+    //mouseenter
+    if(event.type=='mouseenter' && !this.touched && $targets[0] && $targets[0]==event.target) {
+      $targets[0].setAttribute('data-hover', '');
+    }
+    //mouseleave
+    else if(event.type=='mouseleave' && !this.touched && $targets[0] && $targets[0]==event.target) {
+      $targets[0].removeAttribute('data-focus');
+      $targets[0].removeAttribute('data-hover');
+    }
+    //mousedown
+    if(event.type=='mousedown' && !this.touched && $targets[0]) {
+      $targets[0].setAttribute('data-focus', '');
+    } 
+    //mouseup
+    else if(event.type=='mouseup' && !this.touched  && $targets[0]) {
+      $targets[0].removeAttribute('data-focus');
+    }
+  }
+}
